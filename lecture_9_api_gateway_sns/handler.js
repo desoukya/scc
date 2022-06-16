ac5db120a53467dd6e98a560daf4a2b777060cb4
Amr Desouky <desoukya@gmail.com>
2022-06-16 12:17:13 +0200

    add reference to access/secret

--- lecture_9_api_gateway_sns/handler.js
+++ lecture_9_api_gateway_sns/handler.js
@@ -1,13 +1,18 @@
 'use strict';
 const AWS = require('aws-sdk');
-AWS.config.update({region: 'us-east-1'});
+// Configure AWS Access - Make sure your IAM role has full SNS Access
+AWS.config.update({
+  region: 'us-east-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID, /* Insert your IAM AWS Access Key Id */
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, /* Insert your IAM AWS Secret Key */
+});
 const sns = new AWS.SNS({ apiVersion: '2010-03-31' });
 
 module.exports.publishMessage = async (event) => {
   // Note the request body is received as a serialized string under event.body
   const params = {
     Message: event.body, /* required */
-    TopicArn: 'TOPIC_ARN' /* Insert your SNS Topic ARN */
+    TopicArn: 'TOPIC_ARN', /* Insert your SNS Topic ARN */
   };
 
   // Create promise and SNS service object
